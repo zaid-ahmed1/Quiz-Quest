399666f91d55da080a1f018bc5c23ac17e0f2737
bigz88 <zaid.ahmed@ucalgary.ca>
2023-07-18 21:10:20 -0600

    Question and answer integrated with UI.

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,8 +7,18 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,5)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
-    public string getQuestion() {
+    public string GetQuestion() {
         return question;
+    }
+
+    public int GetCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index) {
+        return answers[index];
     }
 }
